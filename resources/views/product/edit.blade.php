1f455c353e3c22c3130a198922fb195a6317ebaa
Rijan Subedi <rijansubedi0@gmail.com>
2025-04-30 08:04:41 +0545

    product action

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
-@section('title', 'Add Product')
+@section('title', 'Edit Product')
 @section('content')
-    <form action="{{route('product.store')}}" method="POST" enctype="multipart/form-data">
+    <form action="{{route('product.update',$product->id)}}" method="POST" enctype="multipart/form-data">
         @csrf
         <select name="category_id" type="text"
             class="border border-gray-300 p-2 rounded-lg w-full mb-4">
@@ -15,36 +15,37 @@
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
         @enderror
         <input type="text" name="name" placeholder="Product Name"
-            class="border border-gray-300 p-2 rounded-lg mb-4 w-full" value="{{ old('name') }}">
+            class="border border-gray-300 p-2 rounded-lg mb-4 w-full" value="{{$product->name}}">
         @error('name')
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
         @enderror
         <input type="text" name="price" placeholder="Price" class="border border-gray-300 p-2 rounded-lg mb-4 w-full"
-            value="{{ old('price') }}">
+            value="{{ $product->price }}">
         @error('price')
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
         @enderror
         <input type="text" name="discounted_price" placeholder="Discounted Price"
-            class="border border-gray-300 p-2 rounded-lg mb-4 w-full" value="{{ old('discounted_price') }}">
+            class="border border-gray-300 p-2 rounded-lg mb-4 w-full" value="{{ $product->discounted_price}}">
         @error('discounted_price')
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
         @enderror
         <input type="text" name="stock" placeholder="Stock" class="border border-gray-300 p-2 rounded-lg mb-4 w-full"
-            value="{{ old('stock') }}">
+            value="{{$product->stock}}">
         @error('stock')
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
         @enderror
         <textarea name="description" placeholder="Product Description" class="border border-gray-300 p-2 rounded-lg w-full mb-4"
-            value="">{{ old('description') }}</textarea>
+           >{{$product->description}}</textarea>
             @error('description')
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
             @enderror
+        <p><img src="{{asset('images/'.$product->photopath)}}" alt="" class="w-32"></p>
         <input type="file" name="photopath" placeholder="Product Image" class="border border-gray-300 p-2 rounded-lg mb-4 w-full">
         @error('photopath')
             <p class="text-red-500 text-sm mb-2 -mt-4">{{ $message }}</p>
         @enderror
         <div class="flex justify-center">
-            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-lg">Create Product</button>
+            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-lg">Update Product</button>
             <a href="{{route('product.index')}}" class="bg-red-600 text-white px-4 py-2 rounded-lg ml-4">Back</a>
         </div>
     </form>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::get('/product', [ProductController::class, 'index'])->name('product.index
 });
 Route::get('/product/create',[ProductController::class,'create'])->name ('product.create');
 Route::post('/product/store',[ProductController::class,'store'])->name ('product.store');
+Route::get('/product/{id}/edit',[ProductController::class,'edit'])->name ('product.edit');
+Route::post('/product/{id}/update',[ProductController::class,'update'])->name ('product.update');
+Route::post('/product/destroy',[ProductController::class,'destroy'])->name ('product.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
